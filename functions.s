6f16ffe8ffdac5a21ed2b4863b2b113911c8fa55
Kevin Mills <kevin.mills226@gmail.com>
2018-12-04 11:26:04 -0500

    Cleanup functions.s.

--- functions.s
+++ functions.s
@@ -1,31 +1,20 @@
 .definelabel itemid_get_heldeffect, 0x0809A924
+.definelabel itemid_get_quality, 0x0809A948
 .definelabel itemid_get_pocket_number, 0x0809A9D8
+
 .definelabel battle_side_get_owner, 0x080751C4
 .definelabel dp01_battle_side_mark_buffer_for_execution, 0x08017248
 .definelabel b_movescr_stack_push, 0x08017520
 .definelabel dp01_build_cmdbuf_x02_a_b_varargs, 0x0800DFF0
-
 .definelabel b_history__record_ability_usage_of_player, 0x080C71D0
 
-.definelabel itemid_get_quality, 0x0809A948
-.definelabel b_call_bc_move_exec, 0x0801BBE4
+.definelabel dislikes_flavor, 0x08044234
+.definelabel move_get_pp_info, 0x0804101C
 
 .definelabel strcpy_xFF_terminated, 0x08008D84
+.definelabel rand, 0x08044EC8
 
-// .definelabel do_itemeffect, 0x080413C0
 .definelabel pokemon_getattr, 0x0803FBE8
-// .definelabel pokemon_setattr, 0x0804037C
-// .definelabel effect_done, 0x080210EC
-// .definelabel item_battle_effects, 0x0801BC68
-// .definelabel expand_battle_string, 0x080D77F4
-// .definelabel battle_show_message_maybe, 0x080D87BC
-// .definelabel case_steal_item, 0x08020CA8
-// .definelabel case_none, 0x0802110A
-
-// .definelabel _script_sel_hook_, 0x08020E40
-// .definelabel script_sel_hook_return, 0x08020E48
-
-// .definelabel effect_complete, 0x0802110A
 
 .definelabel __divsi3, 0x081E4018
 .definelabel __aeabi_idiv, __divsi3
@@ -40,8 +29,3 @@
 
 .definelabel __umodsi3, 0x081E4684
 .definelabel umod, __umodsi3
-
-.definelabel dislikes_flavor, 0x08044234
-.definelabel rand, 0x08044EC8
-
-.definelabel move_get_pp_info, 0x0804101C
